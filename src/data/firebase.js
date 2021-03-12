43c9cdf09c77edff374718f155dd64540c427572
Marie-Lou Denis <marielou33690@gmail.com>
2021-03-12 15:55:50 -0500

    supprimer objet config firebase

--- src/data/firebase.js
+++ src/data/firebase.js
@@ -3,14 +3,14 @@ import 'firebase/firestore';
 
 /******* Ex#3 - Étape B ********************************/ 
 // Ajouter votre objet de configuration Firebase
-const firebaseConfig = {
-  apiKey: "AIzaSyBLZ1MjGR-9naCsqeMjQIppEwkM9qZVMUY",
-  authDomain: "marielou-react.firebaseapp.com",
-  projectId: "marielou-react",
-  storageBucket: "marielou-react.appspot.com",
-  messagingSenderId: "662884818716",
-  appId: "1:662884818716:web:86127ed17d9083dee064d9"
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBLZ1MjGR-9naCsqeMjQIppEwkM9qZVMUY",
+//   authDomain: "marielou-react.firebaseapp.com",
+//   projectId: "marielou-react",
+//   storageBucket: "marielou-react.appspot.com",
+//   messagingSenderId: "662884818716",
+//   appId: "1:662884818716:web:86127ed17d9083dee064d9"
+// };
 
 // Initialiser Firebase
 if(!fbAppli.apps.length) {
